b809734d11f90e491372b0df1048d31d0298ee09
Siuol39 <siuol39100@hotmail.fr>
2016-01-16 10:48:27 +0100

    add file 'file-queue.py', to implement file-saved queues

---
+++ file-queue.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+
+########################################################
+# Implante un système de file stockées dans des fichiers
+########################################################
