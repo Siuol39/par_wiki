e3818d5b77bc72f9c62e7a73c08a89d2f711fe32
Siuol39 <siuol39100@hotmail.fr>
2016-02-17 20:01:22 +0100

    ram_queue : implantation d'une queue en RAM

---
+++ ram_queue.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+
+#############################################
+# Implante un système de file stockées en RAM
+#############################################
+
+class RAMQueue:
+    
